4dbea611079cd14956b087b6964ad4c57afc2a4a
DerekMarmol <dmarmols@miumg.edu.gt>
2024-06-15 07:50:44 -0600

    La clase articulo en Articulo.cs se ha hecho publica para la solución de errores en ArticuloDao

--- p1final2024/Articulo.cs
+++ p1final2024/Articulo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace p1final2024
 {
-    internal class Articulo
+    public class Articulo
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
